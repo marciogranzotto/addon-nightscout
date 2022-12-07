a2eadf03505523abcfd5010161db2192ab4534f8
Benoit Anastay <45088785+BenoitAnastay@users.noreply.github.com>
2022-12-07 23:17:44 +0100

    Cleanup

--- build_local_aarch64.sh
+++
@@ -1,1 +0,0 @@
-cd nightscout && docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base/aarch64:6.1.3" --build-arg BUILD_ARCH=aarch64 --progress plain --no-cache -t nightscout .
--- build_local_amd64.sh
+++
@@ -1,1 +0,0 @@
-cd nightscout && docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base/amd64:6.1.3" --build-arg BUILD_ARCH=amd64 --progress plain --no-cache -t nightscout .
